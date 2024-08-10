927d53bdf6a9f3d9faf47526ebc5b9363e5253ea
Adnan <64307271+Adnann07@users.noreply.github.com>
2024-08-10 18:45:09 +0600

    Add files via upload

---
+++ String_Practise.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s1="adnan";
+    //string s2="miah";
+    //  cout<<s1<<" "<<s2;
+
+    //string s1;
+    //getline(cin,s1);
+    //cout<<s;
+    //str[3];
+
+    //char ch='a';
+    //cout<<int(ch)<<endl;
+
+    //string s="hello";
+    //reverse(s.begin(),s.end());
+    //cout<<s;
+
+    //s.substr(0,3);
+    //position,length;
+    //cout<<s.substr(1);
+
+    //+ is used for concatenation, easy.
+    //or, strcat(s1,s2) eita use hoy character array add korar shomoye
+
+    // string s1="abcd";
+//    char ch='e'
+    //  s1.push_back(ch);
+    //cout<<s1<<endl;
+
+//    cout<<s1.size()<<endl;
+
+//    char ch[20]="abcde";
+    //  cout<<strlen(ch);
+    //int num=432;
+    //string s=to_string(num);
+//   s+="1";
+//    cout<<s[1];
+    //print hobe 4321
+
+
+    //string s;
+    //cin>>s;
+    //sort(s.begin(),s.end());
+    //cout<<s<<endl;
+
+    //string s;
+    //string s(5,'n');
+    //s.append(s1);
+    //cout<<s1[i];
+    //cout<<s.compare(s1);
+    //s.empty();
+    //s.erase(3,2);
+    //cout<<s1.find("com");
+    //s1.insert(2,"no");
+    //string s=s1.substr(6,4);
+    //cout<<s;
+
+    //int i=1234;
+    //string s=to_string(i);
+    //s+="5";
+    // cout<<s;
+
+
+
+}
