a4eeacd2918e82f0da1f3abcc3957771b76ce6d0
Adnan <64307271+Adnann07@users.noreply.github.com>
2024-08-14 00:51:01 +0600

    Add files via upload

---
+++ MaximumMultipleSum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n==3)
+        {
+            cout<<"3"<<endl;
+
+        }
+        if(n>=2 && n!=3)
+        {
+            cout<<"2"<<endl;
+        }
+    }
+}
