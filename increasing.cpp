107c51694c9f39d55f8915420cc39ea21d14b116
Adnan <64307271+Adnann07@users.noreply.github.com>
2024-08-26 10:54:35 +0600

    Add files via upload

---
+++ increasing.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        bool sorthoise=true;
+        long long int n;
+        cin>>n;
+        long long int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+
+        sort(a,a+n);
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==a[i+1])
+            {
+                sorthoise=false;
+            }
+        }
+        if(sorthoise==true)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
