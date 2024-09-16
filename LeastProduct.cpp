bb1d441736d36d83d233d16fd791bcb86cfa5d03
Adnan <64307271+Adnann07@users.noreply.github.com>
2024-09-16 16:04:33 +0600

    Add files via upload

---
+++ LeastProduct.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        long long int a[n];
+
+        int zero=0;
+        int neg=0;
+
+        for(int i=0;i<n;i++)
+        {
+
+            cin>>a[i];
+           if(a[i]==0) zero++;
+           else if(a[i]<0) neg++;
+
+        }
+        if(zero>0 || neg%2!=0) cout<<0<<endl;
+        else
+        {
+            cout<<1<<endl;
+            cout<<1<<" "<<0<<endl;
+        }
+    }
+
+}
